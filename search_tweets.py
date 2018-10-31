8772d98672f909dfacc77dd2cc59412ca7486239
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-10-31 13:27:43 +0530

    tweets using module added

--- search_tweets.py
+++ search_tweets.py
@@ -1,24 +1,7 @@
 from twython import Twython
 import json
 
-'''
-Run this script first to enter your credentials into a json file
 
-import json
-
-# Enter your keys/secrets as strings in the following fields
-credentials = {}  
-credentials['CONSUMER_KEY'] = ...  
-credentials['CONSUMER_SECRET'] = ...  
-credentials['ACCESS_TOKEN'] = ...  
-credentials['ACCESS_SECRET'] = ...
-
-# Save the credentials object to file
-with open("twitter_credentials.json", "w") as file:  
-    json.dump(credentials, file)
-'''
-
-#opens the above created json file and loads into creds
 with open("twitter_credentials.json", "r") as file:
         creds = json.load(file)
 
