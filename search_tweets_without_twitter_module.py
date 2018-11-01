0787e0a9c2ff57597e4b60614a264dd7400b80c9
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-11-01 18:47:25 +0530

    codes cleaned

--- search_tweets_without_twitter_module.py
+++ search_tweets_without_twitter_module.py
@@ -2,7 +2,6 @@ import requests
 import json
 from requests_oauthlib import OAuth1
 
-url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
 
 with open("twitter_credentials.json", "r") as file:
 	creds = json.load(file)
