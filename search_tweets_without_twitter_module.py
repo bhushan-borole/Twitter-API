51fa41ce93dec586689753e8125f280650a97a98
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-11-04 19:07:41 +0530

    without module updated

--- search_tweets_without_twitter_module.py
+++ search_tweets_without_twitter_module.py
@@ -16,14 +16,50 @@ auth = OAuth1(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 
 search = input("Enter Keyword: ")
 
-r = requests.get(url = 'https://api.twitter.com/1.1/search/tweets.json?q=' + search, auth = auth)
+url = 'https://api.twitter.com/1.1/search/tweets.json?q='
+
+r = requests.get(url = url + search + '&count=200', auth = auth)
 
 data = r.json()
+#initalize max_id
+MAX_ID = 0
+
+COUNT = 0
 
 for tweets in data['statuses']:
-        print('User: ' + tweets['user']['screen_name'])
-        print('Created At: ' + tweets['created_at'])
-        print('Text: ' + tweets['text'].translate(non_bmp_map))
-        print(''.join(['-']*15))
+    print('User: ' + tweets['user']['screen_name'])
+    print('Created At: ' + tweets['created_at'])
+    print('Text: ' + tweets['text'].translate(non_bmp_map))
+    max_id = tweets['id']                                          #fetch max_id of the last tweet of the first request
+    print(''.join(['-']*15))
+    COUNT += 1
+
+for i in range(0, 10):
+	try:
+		r1 = requests.get(url = url + search + '&count=200&max_id=' + str(max_id), auth = auth) # pass max_id as the parameter to the request
+
+		data1 = r1.json()
+
+		for tweets in data1['statuses']:
+		    print('User: ' + tweets['user']['screen_name'])
+		    print('Created At: ' + tweets['created_at'])
+		    print('Text: ' + tweets['text'].translate(non_bmp_map))
+		    max_id = tweets['id']                                 
+		    print(''.join(['-']*15))
+		    COUNT += 1
+
+	except Exception as e:
+		print(e)
+
+print(str(COUNT) + ' tweets fetched.')
+
+
+'''
+retrieve max_id of the last tweet of each request and pass it as a parameter to the next request.
+'''
+
+
+
+
 
         
