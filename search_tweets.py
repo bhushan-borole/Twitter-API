989c3d590976a6ca09155286876623791e0cc24f
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-11-09 07:54:23 +0530

    search_tweets updated

--- search_tweets.py
+++ search_tweets.py
@@ -5,50 +5,53 @@ import sys
 #reference for non_nmp_map : https://stackoverflow.com/questions/32442608/ucs-2-codec-cant-encode-characters-in-position-1050-1050
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
-with open("twitter_credentials.json", "r") as file:
-        creds = json.load(file)
+class Twitter_Client:
+    def __init__(self):
+        with open("twitter_credentials.json", "r") as file:
+            creds = json.load(file)
 
-# Instantiate an object
-python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+        # Instantiate an object
+        self.api = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 
-search = input("Enter keyword: ")
-max_attempts = int(input("Enter Max Attempts: "))
+    def get_tweets(self, search, max_attempts, COUNT):
+        tweets = {}
+        #----------------------------------------------------------------#
+        # STEP 1: Query Twitter
+        # STEP 2: Save the returned tweets
+        # STEP 3: Get the next max_id
+        #----------------------------------------------------------------#
 
-COUNT = 3000
-tweets = {}
+        for i in range(0, max_attempts):
+                if COUNT < len(tweets):
+                    break
 
-#----------------------------------------------------------------#
-# STEP 1: Query Twitter
-# STEP 2: Save the returned tweets
-# STEP 3: Get the next max_id
-#----------------------------------------------------------------#
+                if i == 0:
+                    results = self.api.search(q = search, count = '200')
+                else:
+                    results = self.api.search(q = search, include_entities = 'true', max_id = next_max_id)
 
-for i in range(0, max_attempts):
-        if COUNT < len(tweets):
-            break
+                
+                for result in results['statuses']:
+                    tweets[result['user']['screen_name']] = result['text'].translate(non_bmp_map)
+                    next_max_id = result['id']
+        return tweets
+                
 
-        if i == 0:
-            results = python_tweets.search(q = search, count = '200')
-        else:
-            results = python_tweets.search(q = search, include_entities = 'true', max_id = next_max_id)
+def main():
+    search = input("Enter keyword: ")
+    max_attempts = int(input("Enter Max Attempts: "))
+    COUNT = 3000
+    client = Twitter_Client()
+    tweets = client.get_tweets(search, max_attempts, COUNT)
 
+    for k, v in tweets.items():
+        print("User: " + k)
+        print("Tweet: " + v)
+        print(''.join(['-']*15))
         
-        for result in results['statuses']:
-            tweets[result['user']['screen_name']] = result['text'].translate(non_bmp_map)
-            next_max_id = result['id']
-        '''
-
-        try:
-            next_results_url_params = results['search_metadata']['next_results']
-            next_max_id = next_results_url_params.split('max_id=')[1].split('&')[0]  
-        except:
-            break
-        '''
+    print(str(len(tweets)) + " tweets fetched.")
 
 
-for k, v in tweets.items():
-    print("User: " + k)
-    print("Tweet: " + v)
-    print(''.join(['-']*15))
-    
-print(str(len(tweets)) + " tweets fetched.")
+if __name__ == '__main__':
+    main()
+
